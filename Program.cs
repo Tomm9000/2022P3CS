1e93a1f8aace8a9d8cebf593ae25d23c3c5fc299
Tomm9000 <32077@ma-web.nl>
2022-02-16 19:36:27 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2022P3CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
